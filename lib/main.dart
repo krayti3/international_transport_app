99a375085c46b552c2cf292a1650c947b6bbe70e
krayti3 <krayti1aep@gmail.com>
2026-07-26 20:05:40 +0100

    1925

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -9,9 +10,11 @@ import 'package:international_transport_app/screens/login_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  final dotenv = DotEnv();
+  await dotenv.load();
   await Supabase.initialize(
-    url: 'https://jgehdsmrmcpnvcnfrjai.supabase.co',
-    publishableKey: 'sb_publishable_2XqW30G2W9f5ayTuLAZUJw_2zLpWG_G',
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
   runApp(const MyApp());
 }
